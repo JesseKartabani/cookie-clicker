d880e5b9fe0c5c16291410113dfdf7949d8931aa
Jesse1224 <jessekartabani@gmail.com>
2022-10-13 13:32:07 +1100

    Component boilerplate

---
+++ components/Cookie.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Cookie = () => {
+  return (
+    <View>
+      <Text>Cookie</Text>
+    </View>
+  );
+};
+
+export default Cookie;
+
+const styles = StyleSheet.create({});
