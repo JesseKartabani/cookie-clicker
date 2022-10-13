d90e25fd64a791a56ce4274c360bd77e2796fe0d
Jesse1224 <jessekartabani@gmail.com>
2022-10-13 17:20:31 +1100

    Delete Cookie Counter

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Cookie from "./components/Cookie";
-import CookieCounter from "./components/CookieCounter";
 
 export default function App() {
   return (
     <SafeAreaProvider style={styles.container}>
-      <CookieCounter />
       <Cookie />
       {/* GameStore (horizontal) */}
     </SafeAreaProvider>
