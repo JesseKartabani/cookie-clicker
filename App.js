0bdce96c6c3180a3701f18310424129eaec8aac8
Jesse1224 <jessekartabani@gmail.com>
2022-10-13 13:31:41 +1100

    Rough layout planning comments

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <SafeAreaProvider style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>TEST</Text>
+      {/* GameStore (horizontal) */}
+      {/* Clickable cookie */}
+      {/* Cookie counter */}
     </SafeAreaProvider>
   );
 }
