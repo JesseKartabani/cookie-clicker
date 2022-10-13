2e339a7273dc78994de9fecda3170bab0c5025e0
Jesse1224 <jessekartabani@gmail.com>
2022-10-13 16:42:02 +1100

    CookieCounter component boilerplate

---
+++ components/CookieCounter.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React from "react";
+
+const CookieCounter = () => {
+  return (
+    <SafeAreaView>
+      <Text>CookieCounter</Text>
+    </SafeAreaView>
+  );
+};
+
+export default CookieCounter;
+
+const styles = StyleSheet.create({});
