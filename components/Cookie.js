9fe74c6d0eccb0cd6b519cb0ada1e8b0b581388a
Jesse1224 <jessekartabani@gmail.com>
2022-10-13 14:40:32 +1100

    Cookie image and starting styling

--- components/Cookie.js
+++ components/Cookie.js
@@ -1,14 +1,39 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import React from "react";
 
 const Cookie = () => {
   return (
-    <View>
-      <Text>Cookie</Text>
-    </View>
+    <SafeAreaView>
+      <TouchableOpacity>
+        <View style={Styles.img}>
+          <Image
+            style={{
+              resizeMode: "contain",
+              width: 100,
+              height: 100,
+            }}
+            source={{
+              uri: "https://i.imgur.com/D1sOdaz.png",
+            }}
+          />
+        </View>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 };
 
 export default Cookie;
 
-const styles = StyleSheet.create({});
+const Styles = StyleSheet.create({
+  img: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
