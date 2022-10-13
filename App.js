d3d842f731f7ba20f843f5231d3db7a52fb621a5
Jesse1224 <jessekartabani@gmail.com>
2022-10-13 13:35:01 +1100

    Add cookie component into app

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import Cookie from "./components/Cookie";
 
 export default function App() {
   return (
     <SafeAreaProvider style={styles.container}>
       <Text>TEST</Text>
       {/* GameStore (horizontal) */}
-      {/* Clickable cookie */}
+      <Cookie />
       {/* Cookie counter */}
     </SafeAreaProvider>
   );
