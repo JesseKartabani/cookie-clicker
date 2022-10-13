a6c88a3b39689ca2018203cb5b65dcf307cb497f
Jesse1224 <jessekartabani@gmail.com>
2022-10-13 16:45:17 +1100

    Placeholder text

--- components/CookieCounter.js
+++ components/CookieCounter.js
@@ -4,7 +4,8 @@ import React from "react";
 const CookieCounter = () => {
   return (
     <SafeAreaView>
-      <Text>CookieCounter</Text>
+      <Text>Cookies</Text>
+      <Text>per second</Text>
     </SafeAreaView>
   );
 };
