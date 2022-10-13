4f754e75aac31f5cc3fc39dfde3fe2fca81cfa8a
Jesse1224 <jessekartabani@gmail.com>
2022-10-13 16:28:11 +1100

    Cookie shake on click animation

--- components/Cookie.js
+++ components/Cookie.js
@@ -6,19 +6,51 @@ import {
   TouchableOpacity,
   Image,
   Platform,
+  Animated,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 
 const Cookie = () => {
+  const [rotateAnimation, setRotateAnimation] = useState(new Animated.Value(0));
+
+  // Rotates cookie on click
+  const handleAnimation = () => {
+    Animated.timing(rotateAnimation, {
+      toValue: 1,
+      duration: 50, // How long cookie rotates for
+      useNativeDriver: true,
+    }).start(() => {
+      rotateAnimation.setValue(0);
+    });
+  };
+
+  const interpolateRotating = rotateAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "2deg"], // How much cookie rotates
+  });
+
+  const animatedStyle = {
+    transform: [
+      {
+        rotate: interpolateRotating,
+      },
+    ],
+  };
+
   return (
     <SafeAreaView>
-      <TouchableOpacity style={Styles.imgContainer}>
-        <Image
-          style={Styles.img}
-          source={{
-            uri: "https://i.imgur.com/D1sOdaz.png",
-          }}
-        />
+      <TouchableOpacity
+        onPress={async () => handleAnimation()}
+        style={Styles.imgContainer}
+      >
+        <Animated.Text style={animatedStyle}>
+          <Image
+            style={Styles.img}
+            source={{
+              uri: "https://i.imgur.com/D1sOdaz.png",
+            }}
+          />
+        </Animated.Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
